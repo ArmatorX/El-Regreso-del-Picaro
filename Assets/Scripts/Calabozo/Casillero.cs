ffd1707264cad802e3b4e5554e953c72cbe62da2
ArmatorX <fedealejo_007@hotmail.com>
2020-02-05 07:40:55 -0300

    Cambio estructura del calabozo a patrón Composite.

---
+++ Assets/Scripts/Calabozo/Casillero.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Casillero : MonoBehaviour, IComponenteCalabozo
+{
+    private int númeroOrden;
+    private Vector3 posición;
+    private Visibilidad visibilidad;
+    //private ObjetoAgarrable objeto;
+
+    public Casillero(Vector3 posición)
+    {
+        this.posición = posición;
+    }
+
+    /// <summary>
+    /// No se guardan las dimensiones del Casillero.
+    /// </summary>
+    public Vector3 Dimensiones { 
+        get => throw new System.InvalidOperationException("No se guardan las dimensiones del Casillero.");
+        set => throw new System.InvalidOperationException("No se guardan las dimensiones del Casillero."); 
+    }
+    public int NúmeroOrden { get => númeroOrden; set => númeroOrden = value; }
+    public Vector3 Posición { get => posición; set => posición = value; }
+    public Visibilidad Visibilidad { get => visibilidad; set => visibilidad = value; }
+
+    /// <summary>
+    /// No se utiliza en esta clase, dado que no tiene hijos.
+    /// </summary>
+    /// <param name="componente">-</param>
+    public void añadir(IComponenteCalabozo componente)
+    {
+        // No tiene hijos, por lo que no se implementa.
+        throw new System.InvalidOperationException("La clase Casillero no tiene hijos. No se pueden añadir hijos.");
+    }
+
+    /// <summary>
+    /// No se utiliza en esta clase, dado que no tiene hijos.
+    /// </summary>
+    /// <param name="componente">-</param>
+    public void eliminar(IComponenteCalabozo componente)
+    {
+        // No tiene hijos, por lo que no se implementa.
+        throw new System.InvalidOperationException("La clase Casillero no tiene hijos. No se pueden eliminar hijos.");
+    }
+
+    public void iluminar()
+    {
+        this.visibilidad = new Visibilidad(Visibilidad.VISIBILIDAD.Total, this.GetType().Name);
+    }
+
+    public void mostrar()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    /// <summary>
+    /// No se utiliza en esta clase, dado que los casilleros no se
+    /// pueden ver entre sí.
+    /// </summary>
+    /// <param name="casilleroOrigen">-</param>
+    /// <param name="dirección">-</param>
+    /// <returns>-</returns>
+    public Casillero obtenerCasilleroDestino(Casillero casilleroOrigen, Vector3 dirección)
+    {
+        throw new System.InvalidOperationException("Los casilleros no se conocen entre sí.");
+    }
+
+    /// <summary>
+    /// No se utiliza en esta clase, dado que no tiene hijos.
+    /// </summary>
+    /// <param name="númeroOrden">-</param>
+    /// <returns>-</returns>
+    public IComponenteCalabozo obtenerHijo(int númeroOrden)
+    {
+        throw new System.InvalidOperationException("La clase Casillero no tiene hijos. No se pueden obtener hijos.");
+    }
+
+    /// <summary>
+    /// No se utiliza en esta clase, dado que no tiene hijos.
+    /// </summary>
+    /// <param name="posición">-</param>
+    /// <returns>-</returns>
+    public IComponenteCalabozo obtenerHijo(Vector3 posición)
+    {
+        throw new System.InvalidOperationException("La clase Casillero no tiene hijos. No se pueden obtener hijos.");
+    }
+
+    /**
+     * <summary>
+     * Verifica si esta instancia de casillero, es el casillero adyacente al personaje en la dirección del movimiento. 
+     * </summary>
+     * <remarks>
+     * Se usa durante la actualización de la ubicación del personaje, para obtener el casillero de destino.
+     * </remarks>
+     * <param name="casilleroOrigen">La ubicación actual del personaje.</param>
+     * <param name="dirección">La dirección en la cual el personaje se quiere mover.</param>
+     * <returns>Verdadero si el casillero es el casillero destino.</returns>
+     */
+    public bool esCasilleroDestino(Casillero casilleroOrigen, Vector3 dirección)
+    {
+        return (casilleroOrigen.Posición + dirección == this.posición);
+    }
+}
---
+++ Assets/Scripts/Calabozo/IComponenteCalabozo.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Esta interfaz surge de un patrón Composite, y se utiliza para 
+/// comunicarse con los elementos que conforman la estructura del 
+/// Calabozo. Puede representar el calabozo, un mapa, una habitación,
+/// un pasillo, o un casillero.
+/// </summary>
+/// <remarks>
+/// Si bien las interfaces no pueden tener atributos, durante el 
+/// diseño se estableció que era necesario que contuviera algunos 
+/// setters y getters comunes a toda la estructura.
+/// </remarks>
+public interface IComponenteCalabozo 
+{
+    Vector3 Dimensiones { get; set; }
+    Vector3 Posición { get; set; }
+    int NúmeroOrden { get; set; }
+
+    /// <summary>
+    /// Se usa para determinar el casillero al cual se quiere mover
+    /// una entidad.
+    /// </summary>
+    /// <param name="casilleroOrigen">Casillero en el cual se 
+    /// encuentra el personaje.</param>
+    /// <param name="dirección">Dirección en que se quiere mover la 
+    /// entidad</param>
+    /// <returns>Casillero de destino. Es adyacente al de origen. 
+    /// Devuelve null si se usa en una estructura que no contiene 
+    /// casilleros, o si la estructura no contiene el casillero de
+    /// destino.</returns>
+    Casillero obtenerCasilleroDestino(Casillero casilleroOrigen, Vector3 dirección);
+    
+    /// <summary>
+    /// Ilumina un elemento del calabozo, haciéndolo completamente
+    /// visible para el usuario. Maximiza la visibilidad del componente.
+    /// </summary>
+    void iluminar();
+
+    /// <summary>
+    /// Muestra un componente del calobozo (solo la parte que es 
+    /// visible al Jugador).
+    /// </summary>
+    void mostrar();
+
+    /// <summary>
+    /// Añade un componente como hijo al componente padre, como 
+    /// puede ser una habitación a un mapa.
+    /// </summary>
+    /// <param name="componente">Componente a agregar.</param>
+    void añadir(IComponenteCalabozo componente);
+
+    /// <summary>
+    /// Elimina un componente de la estructura.
+    /// </summary>
+    /// <param name="componente">Componente a eliminar.</param>
+    void eliminar(IComponenteCalabozo componente);
+
+    /// <summary>
+    /// Obtiene un hijo del objeto a partir del número de órden.
+    /// </summary>
+    /// <param name="númeroOrden">Número de órden del hijo.</param>
+    /// <returns>Componente del calabozo.</returns>
+    IComponenteCalabozo obtenerHijo(int númeroOrden);
+
+    /// <summary>
+    /// Obtiene un hijo del objeto a partir de su posición.
+    /// </summary>
+    /// <param name="posición">Posición del objeto.</param>
+    /// <returns>Componente del calabozo, o null si no encontró 
+    /// resultados.</returns>
+    IComponenteCalabozo obtenerHijo(Vector3 posición);
+}
